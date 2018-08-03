cc05ddc18b1edb1e0854798866a85fc9aec70de7
Julian Martinez <jumar98@hotmail.com>
2018-08-03 08:48:11 -0500

    Import necessary stuff, for use bootstrap and add eventBus.
    - jquery
    - Set global.jquery
    - bootstrap
    - bootstrap.css

--- condortube/src/main.js
+++ condortube/src/main.js
@@ -1,12 +1,30 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+//Import modules like vue, App, router
+//jquery, bootstrap and bootstrap css
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import jquery from 'jquery';
+global.jquery = jquery;
+let Bootstrap = require('bootstrap');
+import 'bootstrap/dist/css/bootstrap.css';
 
+//Set in the global config, productionTip to 
+//false, to prevent the production tips
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+//Use event bus aproach to manage events 
+//across components
+window.eventBus = new Vue({});
+
+/*
+  Create vue instances, specify main 
+  component, the template, the element
+  id and the routes
+*/  
 new Vue({
   el: '#app',
   router,

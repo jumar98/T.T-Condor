960fa754e3b6daf5399c75ebb0b9a2fa7d345288
Julian Martinez <jumar98@hotmail.com>
2018-08-03 08:16:31 -0500

    Change module main in path '/'

--- condortube/src/router/index.js
+++ condortube/src/router/index.js
@@ -1,15 +1,19 @@
+//Import modules and component main
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import CondorTubeDashboard from '@/components/CondorTubeDashboard'
 
+//Set Router module to use on vue 
 Vue.use(Router)
 
+//Define routes and set component 
+//foreach route
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'CondorTubeDashboard',
+      component: CondorTubeDashboard
     }
   ]
 })

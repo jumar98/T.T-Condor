145c3948a69b0c5c5082719d6d03e1c3f7e16d76
Julian Martinez <jumar98@hotmail.com>
2018-08-03 18:28:15 -0500

    Create info box component

---
+++ condortube/src/components/InfoBox.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="mt-2">
+        <h2>{{videoTitle}}</h2>
+        <p>{{channelTitle}}</p>
+        <small>
+            <p style="color:grey">
+            {{new Intl.NumberFormat().format(statistics.viewCount)}} views
+            {{new Intl.NumberFormat().format(statistics.likeCount)}} likes
+            {{new Intl.NumberFormat().format(statistics.dislikeCount)}} dislikes
+            </p>
+        </small>
+        <small style="color:grey">{{videoDescription}}</small>
+    </div>
+</template>
+
+<script>
+    export default{
+        props:['video','statistics'],
+        
+        computed:{
+            //Return title
+            //Return title
+            videoTitle(){
+                return this.video.snippet.title;
+            },
+            //Return channel title
+            channelTitle(){
+                return this.video.snippet.channelTitle;
+            },
+            //Return video description
+            videoDescription(){
+                return this.video.snippet.description;
+            }
+        }
+    }
+</script>

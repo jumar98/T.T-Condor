d393c0103ce15200273d61b8fff605b227fa89d2
Julian Martinez <jumar98@hotmail.com>
2018-08-03 18:30:06 -0500

    Add info-box component and get statistics data

--- condortube/src/components/VideoDetail.vue
+++ condortube/src/components/VideoDetail.vue
@@ -1,18 +1,20 @@
 <template>
     <!--Show video if exist-->
-    <div class="row" v-if="video">
+    <div class="row container mt-5" v-if="video">
         <div class="col-sm-8 col-sm-push-2">
-            <h2>{{video.snippet.title}}</h2>
-            <small>{{video.snippet.channelTitle}}</small>
-            <p>{{video.snippet.description}}</p>
-        <div class="embed-responsive embed-responsive-16by9">
-            <iframe v-bind:src="url" class="embed-responsive-item" frameborder="0"></iframe>
-        </div>
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe v-bind:src="url" class="embed-responsive-item" frameborder="0"></iframe>
+            </div>
+            <!--Show video details-->
+            <info-box :video="video" :statistics="statistics"></info-box>
         </div>
     </div>
 </template>
 
 <script>
+    //Import module and component necessary
+    import { statistics } from '../search/search';
+    import InfoBox from './InfoBox';
     export default{
         //If id params is undifined, comeback to 
         //condortube-dashboard  
@@ -27,13 +29,30 @@
             this.videoId = this.$route.params.id;
             this.url = `https://youtube.com/embed/${this.videoId}`;
             this.video = this.$route.params.video;
+
+            statistics({
+                key:'AIzaSyASSJNgzHQQDmhotVtZZwUbDGibRw7OQCc',
+                id: this.videoId,
+            }, response => this.handleViewsResult(response));
         },
+        //Set up data
         data(){
             return{
                 videoId: null,
                 video: null,
-                url: null
+                url: null,
+                statistics: null
             }
+        },
+        //Pass result of statistics to statistics data
+        methods:{
+            handleViewsResult(result){
+                this.statistics = result;
+            }
+        },
+        //Set InfoBox component
+        components:{
+            InfoBox
         }
     }
 </script>

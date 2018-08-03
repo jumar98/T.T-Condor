ee6bcff4bf6111fd8d84572e2a935c3ec6079dc0
Julian Martinez <jumar98@hotmail.com>
2018-08-03 08:21:55 -0500

    Add comments, container class and delete stuff unnecessary

--- condortube/src/App.vue
+++ condortube/src/App.vue
@@ -1,23 +1,19 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+<!-- 
+    Set router-view for show component of
+    each route
+-->
+  <div id="app" class="container">
     <router-view/>
   </div>
 </template>
 
+
 <script>
+//Define name for component
 export default {
   name: 'App'
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+

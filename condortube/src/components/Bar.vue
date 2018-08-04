a4daa4b8f21738a3685fd46bc4e835465aba1ad3
Julian Martinez <jumar98@hotmail.com>
2018-08-03 23:10:32 -0500

    Create bar component to go back to videos search

---
+++ condortube/src/components/Bar.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav>
+        <center>
+        <router-link :to="{name: 'condortube-dashboard' }">Go to Search Dashboard</router-link>
+        </center>
+    </nav>
+</template>
+
+<script>
+    export default{
+
+    }
+</script>

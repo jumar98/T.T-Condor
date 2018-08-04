16e49964a5fa5b047cb0696eb0f3206a989a375f
Julian Martinez <jumar98@hotmail.com>
2018-08-03 23:09:48 -0500

    Create recommended item component to show a single video

---
+++ condortube/src/components/RecommendedItem.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="row mb-2">
+        <div class="col-sm-6">
+            <img class="card-img-top" width="120" height="100" v-bind:src="videoImage" v-bind:title="videoTitle">
+        </div>
+        <div class="col-sm-6">
+            <p>{{videoTitle}}</p>
+            <p style="color:grey">{{channelTitle}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        //Define prop video
+        props:['video'],
+        //Set methods on computed to return
+        //video properties
+        computed: {
+            //Return title
+            videoTitle(){
+                return this.video.snippet.title;
+            },
+            //Return channel title
+            channelTitle(){
+                return this.video.snippet.channelTitle;
+            },
+            //Return video url for thumbnails medium size
+            videoImage(){
+                return this.video.snippet.thumbnails.default.url;
+            }
+        }
+    }
+</script>
+

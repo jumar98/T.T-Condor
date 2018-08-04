3a3752355e2413e1e1e13a9f89f71054249299f4
Julian Martinez <jumar98@hotmail.com>
2018-08-04 14:53:18 -0500

    Create schema model of providers

---
+++ providers-api/src/models/providerModel.js
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+//Create schema from collection provider of database
+export const ProviderSchema = new Schema({
+    status:{
+        type: String
+    },
+    fullName:{
+        type: String
+    },
+    city:{
+        type: String
+    },
+    address:{
+        type: String
+    },
+    __v:{
+        type: Number
+    },
+    updatedAt:{
+        type: Date,
+        default: Date.now
+    },
+    specialty:{
+        type: String
+    },
+    email:{
+        type: String
+    },
+    middleName:{
+        type: String
+    },
+    firstName:{
+        type: String
+    },
+    lastName:{
+        type: String
+    },
+    createdBy:{
+        type: String
+    },
+    updatedBy:{
+        type: String
+    },
+    projectedStartDate:{
+        type: Date,
+        default: Date.now
+    },
+    document:{
+        type: String
+    },
+    employerId:{
+        type: Number
+    },
+    providerType:{
+        type: String
+    },
+    staffStatus:{
+        type: String
+    },
+    assignedTo:{
+        type: Number
+    },
+});

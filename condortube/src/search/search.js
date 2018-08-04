31463d6075311c031946c5bae8a9807f3a06ea39
Julian Martinez <jumar98@hotmail.com>
2018-08-03 22:59:36 -0500

    Add module recommended

--- condortube/src/search/search.js
+++ condortube/src/search/search.js
@@ -1,18 +1,16 @@
 //Import axios 
-let axios = require('axios');
+const axios = require('axios');
 //Define url of the Api YouTube
 let urlSearch = "https://www.googleapis.com/youtube/v3/search";
 
+const key = "AIzaSyASSJNgzHQQDmhotVtZZwUbDGibRw7OQCc";
+
 //Define module with option and callback as params
 module.exports.search  = (option, callback) => {
-    if(!option.key){
-        throw new Error("CondorTube search need a key");
-    }
-
     //Define params to do the search
     let params = {
         part: 'snippet',
-        key: option.key,
+        key: key,
         q: option.term,
         maxResults: 10,
         type: 'video'
@@ -32,10 +30,11 @@ module.exports.search  = (option, callback) => {
 
 let urlVideos = "https://www.googleapis.com/youtube/v3/videos";
 
+//Define module for get statistics about single video
 module.exports.statistics = (option, callback) => {
     let params = {
         part: 'statistics',
-        key: option.key,
+        key: key,
         id: option.id,
     };
 
@@ -45,3 +44,20 @@ module.exports.statistics = (option, callback) => {
     })
     .catch(error => console.log(error));
 }
+
+//Define module to get recommended videos about the video chosen
+module.exports.recommended = (option, callback) => {
+    let params = {
+        part: 'snippet',
+        key: key,
+        relatedToVideoId: option.id,
+        maxResults: 10,
+        type: 'video'
+    };
+
+    axios.get(urlSearch, { params })
+    .then(response =>{
+    if(callback){ callback(response.data.items)};
+    })
+    .catch(error => console.log(error));
+} 

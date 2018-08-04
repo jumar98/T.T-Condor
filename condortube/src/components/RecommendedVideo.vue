1a07262ac347c9bc8698e2d6cc0b0a2253bbded4
Julian Martinez <jumar98@hotmail.com>
2018-08-03 23:08:49 -0500

    Create recommended vido component to show max videos recommended

---
+++ condortube/src/components/RecommendedVideo.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="recommended_board" class="container rounded">
+        <p>Reccommended Videos</p>
+        <!--
+            Iterare on videos array and pass it to video prop, 
+            this show recommended video
+        -->
+        <recommended-item v-for="video in videos"
+            :video="video"
+            :key="video.id.videoId"
+        ></recommended-item>
+    </div>
+</template>
+
+<script>
+    import RecommendedItem from './RecommendedItem';
+    export default{
+        props: ['videos'],
+        components:{
+            RecommendedItem
+        }
+    }
+</script>
+
+<style>
+    #recommended_board{
+        background-color:#b0ecde;
+    }
+</style>
